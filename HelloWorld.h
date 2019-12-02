cf6d65fbc5795cf00ebc266eb45803f6836f906a
Carlo Wood <carlo@alinoe.com>
2019-12-02 23:09:54 +0100

    rewrite_header: add module name and desc.

--- HelloWorld.h
+++ HelloWorld.h
@@ -1,3 +1,30 @@
+/**
+ * helloworld-task -- A statefultask-based task for demonstration purposes.
+ *
+ * @file
+ * @brief Declaration of task HelloWorld.
+ *
+ * @Copyright (C) 2019  Carlo Wood.
+ *
+ * RSA-1024 0x624ACAD5 1997-01-26                    Sign & Encrypt
+ * Fingerprint16 = 32 EC A7 B6 AC DB 65 A6  F6 F6 55 DD 1C DC FF 61
+ *
+ * This file is part of helloworld-task.
+ *
+ * Helloworld-task is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Helloworld-task is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with helloworld-task.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "statefultask/AIStatefulTask.h"
 
 namespace task {
